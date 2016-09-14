e938ba40c4bfe59369626b9bae6bb12d6eea396a
jps <me@jspenc.com>
2016-09-14 20:18:17 +0100

    data access working

--- feat/DataAccess.fs
+++ feat/DataAccess.fs
@@ -2,22 +2,14 @@
 open System
 open System.IO
 open FSharp.Data
+open FSharp.Data.JsonExtensions
 
 module DataAccess =
+    type jsonProvider = JsonProvider<"../feat/JsonSchema/FeatureSchema.json", SampleIsList=true>
     let GetFeatureFilePaths directoryPath =         
         Directory.GetFiles(directoryPath, "*.json")        
-    let GetFeatures filePath =
+    let GetFeatures filePath =            
         let fileContent = File.ReadAllText(filePath)
-        let json = JsonValue.Parse(fileContent)// |> JsonValue.Array [| Name; Enabled |]
-        //let features = json |> JsonValue.Array [| features |]
-        match json with
-        | JsonValue.Array [| features |] -> 
-            for feature in features do           
-//                if feature?value <> JsonValue.Null then 
-//                    let name, enabled = feature?Name, feature?Enabled
-                    ()
-        | _ -> ()
-        
-        //let parsed = json 
-        //parsed
-        //fileContent
+        let json = jsonProvider.Load(filePath)
+        let array = json.JsonValue.AsArray()
+        array
